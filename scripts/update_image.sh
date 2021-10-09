c6533483d7d406cfcaa5cace1778314942233f08
budimanjojo <budimanjojo@gmail.com>
2021-10-09 08:35:51 +0000

    lets go

--- scripts/update_image.sh
+++ scripts/update_image.sh
@@ -2,7 +2,7 @@
 version=$(cat upstream/version)
 echo "current upstream version is $version"
 
-files='find upstream/deploy/install/ | grep yaml'
+files='find upstream/deploy/install/ | grep yaml | sort'
 master="master-head"
 manager_image="longhornio\/longhorn-manager"
 ui_image="longhornio\/longhorn-ui"
@@ -11,12 +11,12 @@ engine_image="longhornio\/longhorn-engine"
 mkdir -p manifests/setup manifests/deploy
 for f in $files
 do
-    sed -i "s/${manager_image}:${master}/${manager_image}:${version}/g"
-    sed -i "s/${ui_image}:${master}/${ui_image}:${version}/g"
-    sed -i "s/${engine_image}:${master}/${engine_image}:${version}/g"
-    cp $f manifests/deploy
+    sed -i "s/${manager_image}:${master}/${manager_image}:${version}/g" $f
+    sed -i "s/${ui_image}:${master}/${ui_image}:${version}/g" $f
+    sed -i "s/${engine_image}:${master}/${engine_image}:${version}/g" $f
+    cp $f manifests/deploy/
 done
 
-# Copy crds to their own directory
-cp manifests/deploy/01-namespace.yaml manifests/setup/01-namespace.yaml
-cp manifests/deploy/03-crd.yaml /manifests/setup/02-crds.yaml
+# Move crds to their own directory
+mv manifests/deploy/01-namespace.yaml manifests/setup/01-namespace.yaml
+mv manifests/deploy/03-crd.yaml /manifests/setup/02-crds.yaml
