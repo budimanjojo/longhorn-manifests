63ea376f9b53332e90610d117c71afba06d79e54
budimanjojo <budimanjojo@gmail.com>
2021-10-09 08:20:25 +0000

    lets go

---
+++ scripts/update_image.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+version='cat upstream/version'
+echo "current upstream version is $version"
+
+files='find upstream/deploy/install/ | grep yaml'
+master="master-head"
+manager_image="longhornio\/longhorn-manager"
+ui_image="longhornio\/longhorn-ui"
+engine_image="longhornio\/longhorn-engine"
+
+mkdir -p manifests/setup manifests/deploy
+for f in files
+do
+    sed -i "s/${manager_image}:${master}/${manager_image}:${version}"
+    sed -i "s/${ui_image}:${master}/${ui_image}:${version}"
+    sed -i "s/${engine_image}:${master}/${engine_image}:${version}"
+    cp $f manifests/deploy
+done
+
+# Copy crds to their own directory
+cp manifests/deploy/01-namespace.yaml manifests/setup/01-namespace.yaml
+cp manifests/deploy/03-crd.yaml /manifests/setup/02-crds.yaml
---
+++ scripts/update_kustomization.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+kustomization_files='find manifests/*/kustomization.yaml'
+
+for f in $kustomization_files
+do
+    source_dir='dirname $f'
+    rm $f
+    sh -c 'cd $source_dir && kustomize create --autodetect'
+done
